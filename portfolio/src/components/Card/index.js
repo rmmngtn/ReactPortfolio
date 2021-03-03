45f1dda100491680097aa0c2abfd1f1919d351b1
Remmington Pascone <remmingtonpearce@gmail.com>
2021-03-02 17:17:50 -0800

    portfolio page prints cards for each project. Need to add some more

--- portfolio/src/components/Card/index.js
+++ portfolio/src/components/Card/index.js
@@ -1,26 +1,24 @@
 import React from "react";
-import CardBtn from "../CardBtn";
 import "./style.css";
 
 function Card(props) {
   return (
-    <div
-      className="card"
-      style={{
-        backgroundImage: props.image ? `url(${props.image})` : "none"
-      }}
-    >
-      {!props.image && <i className="fa fa-spinner fa-spin" aria-hidden="true" />}
-      <CardBtn
-        onClick={props.handleBtnClick}
-        data-value="pass"
-      />
-      <CardBtn
-        onClick={props.handleBtnClick}
-        data-value="pick"
-      />
+    <div className="card" >
+      <div className="card-body w-40">
+        <h5 className="card-title">{props.title}</h5>
+        <img className="card-img-top" src={props.img} alt="Project Screenshot"></img>
+        <p className="card-text">{props.description}</p>
+        <a href="https://fierce-forest-22712.herokuapp.com/" className="card-link">Deployed Link</a>
+        <a href="https://github.com/rmmngtn/13_Burger_NodeExpressHandlebars" className="card-link">Github</a>
+      </div>
     </div>
-  );
+  )
 }
 
+
+
+
 export default Card;
+
+
+
--- portfolio/src/pages/Portfolio.js
+++ portfolio/src/pages/Portfolio.js
@@ -1,13 +1,59 @@
 import React from "react";
-
-
+import Container from "../components/Container";
+import Projects from "../components/Projects/projects";
 
 
 
 function Portfolio() {
+    const portfolio = [
+        {   
+            id: 1,
+            title: "Burger Logger",
+            img: "./img/screenshotBurg.png",
+            description: "​This application uses multiple technologies to create burgers and sort them based on whether they've been devoured or not!" ,
+            url: " https://fierce-forest-22712.herokuapp.com/",
+            repo: "https://github.com/rmmngtn/13_Burger_NodeExpressHandlebars"
+
+        },
+        {   
+            id: 2,
+            title: "The Green Thumb",
+            img: "./img/greenthumb.png",
+            description: "​Users login to create their specific virtual garden, complete with pictures and information about each plant." ,
+            url: "https://thegreenthumb.herokuapp.com/",
+            repo: "https://github.com/rmmngtn/theGreenThumb"
+
+        },
+        {   
+            id: 3,
+            title: "Employee Directory",
+            img: "./img/employee.png",
+            description: "​This application provides a directory of employee information that can be searched and sorted by name." ,
+            url: "https://rmmngtn.github.io/EmployeeDirectory/",
+            repo: "https://github.com/rmmngtn/EmployeeDirectory"
+
+        },
+        {   
+            id: 4,
+            title: "Fitness Tracker",
+            img: "./img/fitness.png",
+            description: "​This application builds and tracks your workouts!" ,
+            url: "https://young-taiga-06877.herokuapp.com/",
+            repo: "https://github.com/rmmngtn/17_FitnessTracker"
+
+        },
+       
+       
+    ]
+    
+    
+    
     return (
-        <div>
-            <h1> Please for the love of god print anything.</h1>
+        <div className="portfolio">
+            <Container style={{ minHeight: "80%"}} > 
+            <h1>Portfolio</h1>
+            <Projects data={portfolio} />
+            </ Container>
         </div>
     );
 }
